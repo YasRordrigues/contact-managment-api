0948aaeae21a7bce17ea89c94fd20ecf8d8eaa5a
YasRordrigues <yasno2011@live.com>
2023-08-25 22:57:33 -0300

    CI/CD with infraestructure

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region  = "us-west-1"
+  region     = "us-west-2"
   access_key = var.AWS_ACCESS_KEY
   secret_key = var.AWS_SECRET_KEY
 }
@@ -9,9 +9,16 @@ resource "aws_key_pair" "deployer" {
   public_key = file("deployer_key.pub")
 }
 
-resource "aws_security_group" "app_sg" {
-  name        = "AppSecurityGroup"
-  description = "Allow inbound traffic for our app"
+resource "aws_security_group" "example" {
+  name        = "example"
+  description = "Example security group"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   ingress {
     from_port   = 80
@@ -20,25 +27,44 @@ resource "aws_security_group" "app_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-}
-
-resource "aws_instance" "app_server" {
-  ami           = "513740308471" # use the ID for Amazon Linux 2 LTS or other
-  instance_type = "t2.micro"
-  key_name      = aws_key_pair.deployer.key_name
-  vpc_security_group_ids = [aws_security_group.app_sg.id]
 
   tags = {
-    Name = "AppServer"
+    Name = "example"
   }
 }
 
-output "public_ip" {
-  value = aws_instance.app_server.public_ip
+resource "aws_instance" "example" {
+  ami             = "ami-513740308471"
+  instance_type   = "t2.micro"
+  key_name        = aws_key_pair.deployer.key_name
+  security_groups = [aws_security_group.example.name]
+
+  tags = {
+    Name = "ExemploInstancia"
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt-get update
+              sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
+              curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+              echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+              sudo apt-get update
+              sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+              sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+              sudo chmod +x /usr/local/bin/docker-compose
+              EOF
+}
+
+variable "AWS_ACCESS_KEY" {}
+variable "AWS_SECRET_KEY" {}
+
+output "instance_ip" {
+  value = aws_instance.example.public_ip
 }
